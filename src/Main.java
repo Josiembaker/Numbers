ab5ceae5c5696d97e94c43f3471583426d58eec8
Josie Baker <josiebaker@Josies-MacBook-Air-2.local>
2024-01-31 10:09:17 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int intA = 7;
+        int intB = 35;
+        int intProduct = intA * intB;
+        int intSum = intA + intB;
+        int intQutient = intA / intB;
+        int intDifference = intA - intB;
+        int intModulo = intA % intB;
+
+        System.out.println("The Product of " + intA + " + " + intB + " is " + intProduct );
+    }
+}
